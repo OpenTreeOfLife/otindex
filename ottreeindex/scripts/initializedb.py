ce49b9c88355eff77b3467fb0a405449008152ac
Karen Cranston <karen.cranston@gmail.com>
2016-02-03 14:04:44 -0500

    add models to initalized.py

--- ottreeindex/scripts/initializedb.py
+++ ottreeindex/scripts/initializedb.py
@@ -9,11 +9,12 @@ from pyramid.paster import (
     setup_logging,
     )
 
-from pyramid.scripts.common import parse_vars
-
 from ..models import (
     DBSession,
-    MyModel,
+    Study,
+    Tree,
+    Curator,
+    Otu,
     Base,
     )
 
@@ -29,12 +30,12 @@ def main(argv=sys.argv):
     if len(argv) < 2:
         usage(argv)
     config_uri = argv[1]
-    options = parse_vars(argv[2:])
+    #options = parse_vars(argv[2:])
     setup_logging(config_uri)
-    settings = get_appsettings(config_uri, options=options)
+    settings = get_appsettings(config_uri)
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.create_all(engine)
-    with transaction.manager:
-        model = MyModel(name='one', value=1)
-        DBSession.add(model)
+    #with transaction.manager:
+        #model = MyModel(name='one', value=1)
+        #DBSession.add(model)
