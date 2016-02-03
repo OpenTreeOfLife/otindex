6fad327f6df1397c91ad3263ac1869d36152652f
Karen Cranston <karen.cranston@gmail.com>
2016-02-03 14:03:12 -0500

    tests

--- ottreeindex/tests.py
+++ ottreeindex/tests.py
@@ -6,50 +6,56 @@ from pyramid import testing
 from .models import DBSession
 
 
-class TestMyViewSuccessCondition(unittest.TestCase):
-    def setUp(self):
-        self.config = testing.setUp()
-        from sqlalchemy import create_engine
-        engine = create_engine('postgresql+psycopg2://')
-        from .models import (
-            Base,
-            MyModel,
-            )
-        DBSession.configure(bind=engine)
-        Base.metadata.create_all(engine)
-        with transaction.manager:
-            model = MyModel(name='one', value=55)
-            DBSession.add(model)
+# class TestMyViewSuccessCondition(unittest.TestCase):
+#     def setUp(self):
+#         self.config = testing.setUp()
+#         from sqlalchemy import create_engine
+#         engine = create_engine('postgresql+psycopg2://')
+#         from .models import (
+#             Base,
+#             Study,
+#             Tree,
+#             Curator,
+#             Otu,
+#             )
+#         DBSession.configure(bind=engine)
+#         Base.metadata.create_all(engine)
+#         with transaction.manager:
+#             model = MyModel(name='one', value=55)
+#             DBSession.add(model)
+#
+#     def tearDown(self):
+#         DBSession.remove()
+#         testing.tearDown()
+#
+#     def test_passing_view(self):
+#         from .views import my_view
+#         request = testing.DummyRequest()
+#         info = my_view(request)
+#         self.assertEqual(info['one'].name, 'one')
+#         self.assertEqual(info['project'], 'ottreeindex')
 
-    def tearDown(self):
-        DBSession.remove()
-        testing.tearDown()
 
-    def test_passing_view(self):
-        from .views import my_view
-        request = testing.DummyRequest()
-        info = my_view(request)
-        self.assertEqual(info['one'].name, 'one')
-        self.assertEqual(info['project'], 'ottreeindex')
-
-
-class TestMyViewFailureCondition(unittest.TestCase):
-    def setUp(self):
-        self.config = testing.setUp()
-        from sqlalchemy import create_engine
-        engine = create_engine('postgresql+psycopg2://')
-        from .models import (
-            Base,
-            MyModel,
-            )
-        DBSession.configure(bind=engine)
-
-    def tearDown(self):
-        DBSession.remove()
-        testing.tearDown()
-
-    def test_failing_view(self):
-        from .views import my_view
-        request = testing.DummyRequest()
-        info = my_view(request)
-        self.assertEqual(info.status_int, 500)
+# class TestMyViewFailureCondition(unittest.TestCase):
+#     def setUp(self):
+#         self.config = testing.setUp()
+#         from sqlalchemy import create_engine
+#         engine = create_engine('postgresql+psycopg2://')
+#         from .models import (
+#             Base,
+#             Study,
+#             Tree,
+#             Curator,
+#             Otu,
+#             )
+#         DBSession.configure(bind=engine)
+#
+#     def tearDown(self):
+#         DBSession.remove()
+#         testing.tearDown()
+#
+#     def test_failing_view(self):
+#         from .views import my_view
+#         request = testing.DummyRequest()
+#         info = my_view(request)
+#         self.assertEqual(info.status_int, 500)
