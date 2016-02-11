31fd136ec19ce605941dfb89f494a3eb251dc26e
Jonathan A Rees <jar398@mumble.net>
2016-02-11 13:38:05 -0500

    fix problems with print_otu_file

--- ottreeindex/scripts/create_otu_table.py
+++ ottreeindex/scripts/create_otu_table.py
@@ -46,6 +46,36 @@ def import_csv_file(connection,cursor,table,filename):
         connection.commit()
 
 def print_otu_file(connection,cursor,phy,ott,nstudies=None):
+    filename = "otu_mapping.csv"
+    seen = {}
+    with open (filename,'w') as f:
+        # datafile format is 'ottid'\t'treeid' where treeid is not
+        # the treeid (string) in the nexson, but the treeid (int) from
+        # the database for faster indexing
+        counter = 0
+        f.write('{t},{o}\n'.format(t='id',o='name'))
+        for study_id, n in phy.iter_study_objs():
+            print study_id
+            otu_dict = gen_otu_dict(n)
+            # iterate over the OTUs in the study, collecting
+            # the mapped ones
+            for oid, o in otu_dict.items():
+                ottID = o.get('^ot:ottId')
+                if ottID is not None:
+                    label = o['^ot:originalLabel']
+                    ottname = o.get('^ot:ottTaxonName')
+                    if ottname is None:
+                        ottname = 'unnamed'
+                    if ottID not in seen:
+                        f.write('{t},{o}\n'.format(t=ottID,o=ottname))
+                        seen[ottID] = ottname
+            counter+=1
+            if (nstudies and counter>=nstudies):
+                f.close()
+                break
+    return filename
+
+def print_tree_otu_file(connection,cursor,phy,ott,nstudies=None):
     filename = "tree_otu_mapping.csv"
     with open (filename,'w') as f:
         # datafile format is 'ottid'\t'treeid' where treeid is not
@@ -60,14 +90,15 @@ def print_otu_file(connection,cursor,phy,ott,nstudies=None):
             # iterate over the OTUs in the study, collecting
             # the mapped ones
             for oid, o in otu_dict.items():
-                label = o['^ot:originalLabel']
-                ottname = o.get('^ot:ottTaxonName')
-                if ottname is not None:
-                    ottID = o.get('^ot:ottId')
+                ottID = o.get('^ot:ottId')
+                if ottID is not None:
+                    label = o['^ot:originalLabel']
+                    ottname = o.get('^ot:ottTaxonName')
+                    if ottname is not None:
+                        ottname = 'unnamed'
                     otu_props = [ottname,ottID]
                     mapped_otus[oid]=otu_props
                     #print oid,ottID,label,ottname
-
             # now iterate over trees and collect OTUs used in
             # each tree
             for trees_group_id, tree_label, tree in iter_trees(n):
@@ -79,15 +110,13 @@ def print_otu_file(connection,cursor,phy,ott,nstudies=None):
                     if oid is not None:
                         otu_props = mapped_otus.get(oid)
                         if otu_props is not None:
-                            ottname = otu_props[0]
                             ottID = otu_props[1]
                             ottIDs[ottID] = True
-                            #print tree_label,oid,ottID,ottname
+                            #print tree_label,oid,ottID
                             f.write('{t},{o}\n'.format(t=tree_id,o=ottID))
                 # ottIDs = parent_closure(ottIDs,ott)
                 for ottID in ottIDs:
                     f.write('{t},{o}\n'.format(t=tree_id,o=ottID))
-
             counter+=1
             if (nstudies and counter>=nstudies):
                 f.close()
@@ -133,8 +162,11 @@ if __name__ == "__main__":
     connection, cursor = setup_db.connect(config_dict)
     phy = create_phylesystem_obj()
     try:
-        filename = print_otu_file(connection,cursor,phy,ott,args.nstudies)
         OTUTABLE = config_dict['tables']['otutable']
-        import_csv_file(connection,cursor,OTUTABLE,filename)
+        TREEOTUTABLE = config_dict['tables']['treeotutable']
+        otu_filename = print_otu_file(connection,cursor,phy,ott,args.nstudies)
+        tree_otu_filename = print_tree_otu_file(connection,cursor,phy,ott,args.nstudies)
+        import_csv_file(connection,cursor,OTUTABLE,otu_filename)
+        import_csv_file(connection,cursor,TREEOTUTABLE,tree_otu_filename)
     except psy.Error as e:
         print e.pgerror
