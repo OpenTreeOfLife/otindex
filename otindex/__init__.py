9c05369d650044543830d5c01d1af8248bbef085
Emily Jane McTavish <ejmctavish@gmail.com>
2018-06-28 13:52:16 -0700

    alt attempt

--- otindex/__init__.py
+++ otindex/__init__.py
@@ -5,6 +5,7 @@ from sqlalchemy import engine_from_config
 # from paste.translogger import TransLogger
 import logging
 import yaml
+from otindex.cors import add_cors_preflight_handler
 
 from .models import (
     DBSession,
@@ -14,30 +15,6 @@ from .models import (
 
 _LOG = logging.getLogger(__name__)
 
-def request_factory(environ):
-    """Factory function that adds the headers necessary for Cross-domain calls.
-
-    Adapted from:
-       http://stackoverflow.com/questions/21107057/pyramid-cors-for-ajax-requests
-    Copied from mtholder/pyraphyletic
-    """
-    request = Request(environ)
-    _LOG.debug('request factory, .is_xhr {x}'.format(x=request.is_xhr))
-    _LOG.debug('request body: {b}'.format(b=request.body))
-#    _LOG.debug('request factory, .context {c}'.format(c=request.context))
-    if request.is_xhr:
-        _LOG.debug('cross origin request')
-        request.response = Response()
-        request.response.headerlist = []
-        request.response.headerlist.extend(
-            (
-                ('Access-Control-Allow-Origin', '*'),
-                ('Access-Control-Allow-Credentials', 'true'),
-                ('Access-Control-Max-Age', 86400),
-                ('Content-Type', 'application/json')
-            )
-        )
-    return request
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
@@ -46,6 +23,11 @@ def main(global_config, **settings):
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
     config = Configurator(settings=settings)
+    config.include('cors')
+    
+     # make sure to add this before other routes to intercept OPTIONS
+    config.add_cors_preflight_handler()
+
     config.include('pyramid_chameleon')
     config.set_request_factory(request_factory)
     config.add_static_view('static', 'static', cache_max_age=3600)
