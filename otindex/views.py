c6057523da2c26629f98cee27325e4ba3caa00f6
snacktavish <ejmctavish@gmail.com>
2023-07-10 11:20:59 -0700

    pulling in code from web2py phylesystemAPI

--- otindex/views.py
+++ otindex/views.py
@@ -220,7 +220,6 @@ def add_update_studies(request):
         _msg="No payload provided"
         raise HTTPBadRequest(body=_msg)
 
-# payload can be a list of URLs (oti syntax) or a list of
 # study ids
 @view_config(route_name='remove', renderer='json', request_method='POST')
 def remove_studies(request):
@@ -245,3 +244,230 @@ def remove_studies(request):
     else:
         _msg="No payload provided"
         raise HTTPBadRequest(body=_msg)
+
+@view_config(route_name='parse_webhook', renderer='json', request_method='POST')
+def parse_webhook(request):
+    if (request.body):
+        payload = request.json_body
+
+    # get list of study ids to be added / modified / removed
+    try:
+        # how we nudge the index depends on which studies are new, changed, or deleted
+        added_study_ids = [ ]
+        modified_study_ids = [ ]
+        removed_study_ids = [ ]
+        # TODO: Should any of these lists override another? maybe use commit timestamps to "trump" based on later operations?
+        for commit in payload['commits']:
+            _harvest_study_ids_from_paths( commit['added'], added_study_ids )
+            _harvest_study_ids_from_paths( commit['modified'], modified_study_ids )
+            _harvest_study_ids_from_paths( commit['removed'], removed_study_ids )
+
+        # add and update treated the same, so merge
+        # also "flatten" each list to remove duplicates
+        add_or_update_ids = added_study_ids + modified_study_ids
+        add_or_update_ids = list(set(add_or_update_ids))
+        remove_ids = list(set(removed_study_ids))
+
+    except:
+        raise HTTP(400,json.dumps({"error":1, "description":"malformed GitHub payload"}))
+
+    if payload['repository']['url'] != opentree_docstore_url:
+        raise HTTP(400,json.dumps({"error":1, "description":"wrong repo for this API instance"}))
+
+    
+    msg = ''
+    if len(add_or_update_ids) > 0:
+            failed_studies = []
+            updated_studies = []
+        for study in add_or_update_ids:
+            try:
+                aus.update_study(study)
+            except Exception as e:
+                failed_studies.append(study)
+                _LOG.debug('failed to update study {s}'.format(s=study))
+                _LOG.debug(str(e))
+            else:
+                updated_studies.append(study)
+                _LOG.debug('updated study {s}'.format(s=study))
+        results = {
+            "failed_studies" : failed_studies,
+            "updated_studies" : updated_studies
+            }
+        return results
+    else:
+        _msg="add_or_update failure"
+        raise HTTPBadRequest(body=_msg)
+    if len(remove_ids) > 0:
+        for study in remove_ids:
+            try:
+                aus.remove_study(study)
+            except:
+                failed_studies.append(study)
+                _LOG.debug('failed to remove study {s}'.format(s=study))
+            else:
+                updated_studies.append(study)
+                _LOG.debug('removed study {s}'.format(s=study))
+        results = {
+            "failed_studies" : failed_studies,
+            "updated_studies" : updated_studies
+            }
+        return results
+
+    # Clear any cached study lists (both verbose and non-verbose)
+    api_utils.clear_matching_cache_keys(".*find_studies.*")
+
+    github_webhook_url = "%s/settings/hooks" % opentree_docstore_url
+    full_msg = """This URL should be called by a webhook set in the docstore repo:
+    <br /><br />
+    <a href="%s">%s</a><br />
+    <pre>%s</pre>
+    """ % (github_webhook_url, github_webhook_url, msg,)
+    if msg == '':
+        return full_msg
+    else:
+        raise HTTP(500, full_msg)
+
+
+
+
+
+
+# #--------------------------- This is from web2py phylesystemapi--------
+# def nudgeTaxonIndexOnUpdates():
+#     """"Support method to update taxon index (taxomachine) in response to GitHub webhooks
+
+# This examines the JSON payload of a GitHub webhook to see which taxa have
+# been added, modified, or removed. Then it calls the appropriate index service to
+# (re)index these taxa, or to delete a taxon's information if it was deleted in
+# an amendment.
+
+# TODO: Clear any cached taxon list.
+
+# N.B. This depends on a GitHub webhook on the taxonomic-amendments docstore!
+# """
+#     if not check_not_read_only():
+#         raise HTTP(500, "should raise from check_not_read_only")
+#     amendments_repo_url = _read_from_local_config(request, "apis", "amendments_repo_url")
+#     payload = request.vars
+#     if payload['repository']['url'] != amendments_repo_url:
+#         raise HTTP(400,json.dumps({"error":1, "description":"wrong repo for this API instance"}))
+
+#     msg = ''
+#     try:
+#         # how we nudge the index depends on which taxa are new, changed, or deleted
+#         added_ott_ids = [ ]
+#         modified_ott_ids = [ ]
+#         removed_ott_ids = [ ]
+#         # TODO: Should any of these lists override another? maybe use commit timestamps to "trump" based on later operations?
+#         for commit in payload['commits']:
+#             _harvest_ott_ids_from_paths( commit['added'], added_ott_ids )
+#             _harvest_ott_ids_from_paths( commit['modified'], modified_ott_ids )
+#             _harvest_ott_ids_from_paths( commit['removed'], removed_ott_ids )
+#         # "flatten" each list to remove duplicates
+#         added_ott_ids = list(set(added_ott_ids))
+#         modified_ott_ids = list(set(modified_ott_ids))
+#         removed_ott_ids = list(set(removed_ott_ids))
+#     except:
+#         raise HTTP(400,json.dumps({"error":1, "description":"malformed GitHub payload"}))
+
+#     # build a working URL, gather amendment body, and nudge the index!
+#     amendments_api_base_url = api_utils.get_amendments_api_base_url(request)
+
+#     if len(added_ott_ids) > 0:
+#         nudge_url = "{b}v3/taxonomy/process_additions".format(b=amendments_api_base_url)
+
+#         for ott_id in added_ott_ids:
+#             # fetch the JSON body of each new amendment and submit it for indexing
+#             fetch_url = "{b}v3/amendment/{i}".format(b=amendments_api_base_url, i=ott_id)
+#             fetch_response = None
+#             req = urllib2.Request(
+#                 url=fetch_url
+#             )
+#             try:
+#                 fetch_response = urllib2.urlopen(req).read()
+#                 # strip away metadata (version history, etc.)
+#                 amendment_blob = json.loads( fetch_response ).get('data')
+#             except Exception, e:
+#                 # bail and report the error in webhook response
+#                 exc_type, exc_value, exc_traceback = sys.exc_info()
+#                 msg += """fetch of amendment failed!'
+#     fetch_url: %s
+#     fetch_response: %s
+#     ott_id: %s
+#     %s""" % (fetch_url, fetch_response, ott_id, traceback.format_exception(exc_type, exc_value, exc_traceback),)
+#                 break
+
+#             # Extra weirdness required here, as neo4j needs an encoded *string*
+#             # of the amendment JSON, within a second JSON wrapper :-/
+#             POST_blob = {"addition_document": json.dumps(amendment_blob) }
+#             POST_string = json.dumps(POST_blob)
+#             nudge_response = None
+#             req = urllib2.Request(
+#                 url=nudge_url,
+#                 data=POST_string,
+#                 headers={"Content-Type": "application/json"}
+#             )
+#             try:
+#                 # N.B. we don't expect anything interesting here, probably just an empty dict
+#                 nudge_response = urllib2.urlopen(req).read()
+#             except Exception, e:
+#                 # report the error in webhook response
+#                 exc_type, exc_value, exc_traceback = sys.exc_info()
+#                 msg += """index amendments failed!'
+#     nudge_url: %s
+#     POST_data: %s
+#     fetch_response: %s
+#     nudge_response: %s
+#     added_ott_ids: %s
+#     %s""" % (nudge_url, POST_data, fetch_response, nudge_response, added_ott_ids, traceback.format_exception(exc_type, exc_value, exc_traceback),)
+
+#     # LATER: add handlers for modified and removed taxa?
+#     if len(modified_ott_ids) > 0:
+#         raise HTTP(400,json.dumps({
+#             "error":1,
+#             "description":"We don't currently re-index modified taxa!"}))
+#     if len(removed_ott_ids) > 0:
+#         raise HTTP(400,json.dumps({
+#             "error":1,
+#             "description":"We don't currently re-index removed taxa!"}))
+
+#     # N.B. If we had any cached amendment results, we'd clear them now
+#     #api_utils.clear_matching_cache_keys(...)
+
+#     github_webhook_url = "%s/settings/hooks" % amendments_repo_url
+#     full_msg = """This URL should be called by a webhook set in the amendments repo:
+#                     <br /><br />
+#                     <a href="%s">%s</a><br />
+#                     <pre>%s</pre>
+#         """ % (github_webhook_url, github_webhook_url, msg,)
+#     if msg == '':
+#         return full_msg
+#     else:
+#         raise HTTP(500, full_msg)
+
+# def _read_from_local_config(request, section_name, key_name):
+#     app_name = request.application
+#     conf = SafeConfigParser(allow_no_value=True)
+#     if os.path.isfile("%s/applications/%s/private/localconfig" % (os.path.abspath('.'), app_name,)):
+#         conf.read("%s/applications/%s/private/localconfig" % (os.path.abspath('.'), app_name,))
+#     else:
+#         conf.read("%s/applications/%s/private/config" % (os.path.abspath('.'), app_name,))
+#     return conf.get(section_name, key_name)
+
+# def _harvest_study_ids_from_paths( path_list, target_array ):
+#     for path in path_list:
+#         path_parts = path.split('/')
+#         if path_parts[0] == "study":
+#             # skip any intermediate directories in docstore repo
+#             study_id = path_parts[ len(path_parts) - 2 ]
+#             target_array.append(study_id)
+
+# def _harvest_ott_ids_from_paths( path_list, target_array ):
+#     for path in path_list:
+#         path_parts = path.split('/')
+#         # ignore changes to counter file, other directories, etc.
+#         if path_parts[0] == "amendments":
+#             # skip intermediate directories in docstore repo
+#             amendment_file_name = path_parts.pop()
+#             ott_id = amendment_file_name[:-5]
+#             target_array.append(ott_id)
